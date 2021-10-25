5212204b09e653e08404938c796af8919a0b0aa1
shannon_p <snp1284kasey@aol.com>
2021-10-25 15:02:42 -0400

    adding react hooks

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
-import React from 'react';
-import './App.css';
+import React, {useState} from 'react';
 import Nav from './components/Nav';
 import About from './components/About';
+import Gallery from './components/Gallery';
 
 
 function App() {
+  const [categories] = useState([
+    {
+            name: "commercial",
+            description: "Photos of grocery store, food trucks, and other commercial projects",
+        },
+        { name: "portraits", description: "Portraits of other people in my life" },
+        { name: "food", description: "Delicious delicacies" },
+        { name: "landscape", description: "Fields, farmhouses, waterfalls, and the beauty of the nature"},
+  ]);
+
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
   return (
     <div >
-      <Nav></Nav>
+      <Nav>
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
+      </Nav>
       <main>
+        <Gallery></Gallery>
         <About></About>
       </main>
     </div>
