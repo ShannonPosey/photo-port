aef5b1f1c2c67105566305ddcc5de3ca7231600c
shannon_p <snp1284kasey@aol.com>
2021-10-25 16:48:26 -0400

    tested gallery and photolist

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -3,12 +3,21 @@ import {render, cleanup} from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import Nav from "..";
 
+const categories = [
+    { name: "portraits", description: "Portraits of people in my life"}
+];
+
+const mockCurrentCategory = jest.fn();
+const mockSetCurrentCategory = jest.fn();
+
 afterEach(cleanup);
 
 describe("Nav component", () => {
     // baseline test
     it("renders", () => {
-        render(<Nav/>);
+        render(<Nav categories={categories}
+    setCurrentCategory={mockSetCurrentCategory}
+    currentCategory={mockCurrentCategory}/>);
     });
 
     // snapshot test
@@ -36,6 +45,6 @@ describe("links are visible", () => {
 
         // Assert
         expect(getByTestId("link")).toHaveTextContent("Oh Snap!");
-        expect(getByTestId("about")).toHaveTextContent("About Me");
+        expect(getByTestId("about")).toHaveTextContent("About me");
     });
 });
